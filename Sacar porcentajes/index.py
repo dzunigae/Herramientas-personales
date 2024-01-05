acff67ac05fcf15003407ae44d01027c9f69e981
Daniel Zuñiga <1danielarmando.1999@gmail.com>
2024-01-05 17:34:33 -0500

    Herramienta para hallar porcentajes de números

---
+++ Sacar porcentajes/index.py
@@ -0,0 +1,15 @@
+def calcular_porcentaje(numero, total):
+    try:
+        porcentaje = (numero / total) * 100
+        return porcentaje
+    except ZeroDivisionError:
+        return "El denominador no puede ser cero"
+
+while True:
+    total = float(input("Ingrese el número que representa el 100%: "))
+    
+    numero = float(input("Ingrese el número para calcular el porcentaje: "))
+    
+    resultado = calcular_porcentaje(numero, total)
+    print(f"{numero} representa el {resultado}% de {total}.")
+
