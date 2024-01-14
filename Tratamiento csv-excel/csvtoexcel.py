bb633aba0f4a9f77b446e713b95fa36a5248dc5c
Daniel Zuñiga <1danielarmando.1999@gmail.com>
2024-01-13 19:41:30 -0500

    Modificación csvtoexcel.py

--- Tratamiento csv-excel/csvtoexcel.py
+++ Tratamiento csv-excel/csvtoexcel.py
@@ -1,28 +1,37 @@
 import pandas as pd
 import os
 
-RUTA = './Tratamiento csv-excel/assets'
+RUTA = './Tratamiento csv-excel'
+ARCHIVOS_PROBLEMATICOS = []
 
 def csv_a_excel(RUTA):
     # Listar archivos CSV en la carpeta
-    ARCHIVOS_CSV = [archivo for archivo in os.listdir(RUTA) if archivo.endswith('.csv')]
+    ARCHIVOS_CSV = [archivo for archivo in os.listdir(RUTA+'/assets') if archivo.endswith('.csv')]
     
     # Iterar sobre los archivos CSV
     for archivo in ARCHIVOS_CSV:
-        # Construir la ruta completa del archivo CSV
-        ruta_csv = os.path.join(RUTA, archivo)
-        
-        # Construir la ruta completa del archivo Excel
-        archivo_excel = os.path.join(RUTA, archivo.replace('.csv', '.xlsx'))
+        try: 
+            # Construir la ruta completa de los archivos CSV
+            ruta_csv = os.path.join(RUTA+'/assets', archivo)
+            
+            # Construir la ruta completa de los archivos Excel
+            archivo_excel = os.path.join(RUTA+'/out', archivo.replace('.csv', '.xlsx'))
 
-        # Leer datos desde el archivo CSV
-        df = pd.read_csv(ruta_csv)
+            # Leer datos desde el archivo CSV
+            df = pd.read_csv(ruta_csv)
 
-        # Guardar los datos en un archivo de Excel
-        df.to_excel(archivo_excel, index=False)
+            # Guardar los datos en un archivo de Excel
+            df.to_excel(archivo_excel, index=False)
 
-        print(f"Archivo CSV '{ruta_csv}' convertido a Excel correctamente.")
-        
+            print(f"Archivo CSV '{ruta_csv}' convertido a Excel correctamente.")
+
+        except Exception as e:
+            print(f"Ocurrió un error inesperado: {e} en {ruta_csv}")
+
+            ARCHIVOS_PROBLEMATICOS.append(ruta_csv)
+
+    for i in ARCHIVOS_PROBLEMATICOS:
+        print(i)
 
 # Reemplaza "carpeta_csv" y "carpeta_excel" con las rutas de tus carpetas
 csv_a_excel(RUTA)
